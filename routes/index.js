319d2dbfb4b1523e8f77bd250b81ea7fb71f475c
Orlando Lucero <orlando.lucero.camacho@gmail.com>
2022-11-25 23:23:16 -0600

    Se agregó el código para mostrar el login, aún es necesario el crear la funcionalidad

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
+//const practicasController = require("../controllers/practicasController")
 //const htmlController = require("../controllers/htmlController")
 
 /* GET home page. */
+//router.get('/', practicasController.login);
 router.get('/', function(req,res,next) {
-  res.send("Bienvenido a la biblioteca");
+  //res.send("Bienvenido a la biblioteca");
+  res.render('index');
 });
 //router.get('/', htmlController.index);
 
